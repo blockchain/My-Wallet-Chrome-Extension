554aca38a831de400d0b1d832fbb5f074e8d6f5b
Ben Reeves <zootreeves@gmail.com>
2014-12-16 15:03:08 +0000

    Sync Local

--- package.sh
+++ package.sh
@@ -3,7 +3,7 @@
 cd ./extension
 
 WEB_CONTENT="$HOME/Sites/blockchain.info/website/WebContent"
-DOWNLOAD_HTML=true;
+DOWNLOAD_HTML=false;
 RESOURCE_DIR="$WEB_CONTENT/Resources"
 
 if $DOWNLOAD_HTML ; then
