37ad8451f800995280a6be718a49f25b2293da44
Ben Reeves <zootreeves@gmail.com>
2015-01-01 22:57:00 +0000

    Sync Local

--- package_prod.sh
+++ package_prod.sh
@@ -1,6 +1,10 @@
 ./package.sh $HOME/Sites/blockchain.info/website/WebContent true true false
 
-zip -r ./Extension.zip *
+cd ./extension
+
+zip -r ../Extension.zip *
+
+cd ..
 
 if $1 ; then
 	git commit -a -m "Sync Local"
